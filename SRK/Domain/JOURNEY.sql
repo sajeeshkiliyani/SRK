48e9c0c0599e9ebba730fee099c2736ec51c01ed
sajeeshkiliyani <sajeeshk@arssoftware.com>
2015-08-28 13:42:40 +0200

    SQLs

---
+++ SRK/Domain/JOURNEY.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE JOURNEY (
+	Id INT NOT NULL,
+	NaamSchip VARCHAR(255),
+	Z CHAR(1),
+	A CHAR(1),
+	S CHAR(1),
+	I CHAR(1),
+	L CHAR(1),
+	CallSign VARCHAR(255),
+	PRIMARY KEY (Id)
+) ENGINE=InnoDB;
+
